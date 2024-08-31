9c61744ed071419d942e5d108e8eb0f42a9936bf
Otcocuka <d.ipharziev@gmail.com>
2024-08-31 09:42:48 +0400

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-import { Collapse, Modal, Ripple, initTWE } from "tw-elements";
+import { Collapse, Modal, Ripple, initTWE, Tab} from "tw-elements";
 
-initTWE({ Collapse, Modal, Ripple });
+initTWE({ Collapse, Modal, Ripple, Tab });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,4 @@ module.exports = {
     "./node_modules/tw-elements/js/**/*.js"
   ],
   plugins: [require("tw-elements/plugin.cjs")],
-  darkMode: "class"
 };
