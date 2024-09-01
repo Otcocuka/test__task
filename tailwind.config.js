5aee4eb23d01627cdf89cacd42b04457188c8c67
Otcocuka <d.ipharziev@gmail.com>
2024-09-01 21:39:09 +0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./**/*.{html,js}",
     "./node_modules/tw-elements/js/**/*.js"
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sf-pro': ['SF Pro Text', 'sans-serif'],
+        'sf-pro-r': ['SF Pro Rounded', 'sans-serif'],  // Добавляем кастомные шрифты
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+    },
+  },
   plugins: [require("tw-elements/plugin.cjs")],
-  
 };
